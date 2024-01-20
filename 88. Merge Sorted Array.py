cb2e18396ba05f259343d9eba88aa3e730299a16
Víctor Gomenza <viguitars738@gmail.com>
2024-01-19 21:34:53 -0300

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -47,5 +47,38 @@ Follow up: Can you come up with an algorithm that runs in O(m + n) time? """
 
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        nums1.length = m + n
-        nums2.length = n
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        nums1[: p2 + 1] = nums2[: p2 + 1]
+
+
+mySolution1 = Solution()
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+mySolution1.merge(nums1, m, nums2, n)  # Output: [1,2,2,3,5,6]
+
+mySolution2 = Solution()
+nums1 = [1]
+m = 1
+nums2 = []
+n = 0
+mySolution2.merge(nums1, m, nums2, n)  # Output: [1]
+
+mySolution3 = Solution()
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
+mySolution3.merge(nums1, m, nums2, n)  # Output: [1]
